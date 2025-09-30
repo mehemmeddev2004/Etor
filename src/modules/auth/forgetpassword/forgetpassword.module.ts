6aa4518eef33f3e6e6c1e1774831242fa57fd5f9
MirMehemmed Ezizov <your-email@example.com>
2025-10-01 00:31:45 +0400

    Fix ForgetPasswordService dependency injection
    - Added JwtModule import to forgetPasswordModule
    - Added userActivationEntity to TypeORM imports
    - Configured JwtModule with proper secret and expiration
    - Fixed dependency injection error preventing deployment

--- src/modules/auth/forgetpassword/forgetpassword.module.ts
+++ src/modules/auth/forgetpassword/forgetpassword.module.ts
@@ -1,12 +1,24 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { userEntity } from "src/database/user.entity";
-import { userService } from "src/modules/user/user.service";
+import { userActivationEntity } from "src/database/userActication.entity";
 import { ForgetPasswordService } from "./forgetpassword.service";
 import { forgetPasswordController } from "./forgetpassword.controller";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([userEntity])],
+    imports:[
+        TypeOrmModule.forFeature([userEntity, userActivationEntity]),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: { expiresIn: '7d' },
+            }),
+        }),
+    ],
     providers:[ForgetPasswordService],
     controllers:[forgetPasswordController]
 })
